a4bc290c9a78da884d5b9d0f04a66fdf427c59a1
wangh5 <wangh5@m2600161857.v41.uthscsa.edu>
2025-09-25 17:35:09 -0500

    Fix base to use relative paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,22 +10,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  // Map vite mode to base path
-  const base =
-    mode === "staging" ? "/acs-dashboard/preview/" : "/acs-dashboard/";
-
   return {
     plugins: [react()],
-    base,
+    //  no leading slash, just relative
+    base: "./",
 
     server: {
       port: 5173,
       open: true,
-      historyApiFallback: true, // allows SPA routing
+      historyApiFallback: true,
     },
 
     test: {
-      environment: "jsdom", // gives document/window in tests
+      environment: "jsdom",
       globals: true,
     },
   };
